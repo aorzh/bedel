6e6435f98a95774e4dec5af96a9a4c3a095f3389
Alex Orzheshkovsky <al.orzh@gmail.com>
2014-10-21 06:27:50 +0300

    Belgium module alfa

--- sites/all/modules/belgium/js/script.js
+++ sites/all/modules/belgium/js/script.js
@@ -2,48 +2,89 @@
 
 
 // To understand behaviors, see https://drupal.org/node/756722#behaviors
-    Drupal.behaviors.my_custom_behavior = {
+    Drupal.behaviors.belgium = {
         attach: function (context, settings) {
+            var population = settings.belgium.population;
+            var cityWrap = '<div class="city-wrap"><ul class="city-wrap-list"></ul></div>'
 
-            // Place your code here.
+            //Traditions list
+            var traditions = settings.belgium.traditions;
+            var tradWrap = '<div class="trad-wrap"><ul class="trad-wrap-list"></ul></div>';
+            $('#block-belgium-map').prepend(tradWrap);
+            $.each(traditions, function (key, value) {
+                var link = value.toLowerCase();
+                $('.trad-wrap-list').append('<li><a name="' + key + '" href="#' + link + '">' + value + '</a></li>');
+            });
+
+            //Try to send Tradition ID (key) to php
+            $('.trad-wrap-list').find('a').click(function () {
+                $('.city-wrap-list').empty();
+                var key = $(this).attr('name');
+
+                var post = "&trad_id=" + key;
+                $.ajax({
+                    'url': 'ajax/get_info',
+                    'type': 'POST',
+                    'dataType': 'json',
+                    'data': post,
+                    'success': function (data) {
+                        $.each(data, function (key, value) {
+                            var peoples = population[key];
+                            var singers = data[key];
+                            var points = (singers/peoples)*100;
+                            //console.log(points);
+
+                            var link = key.toLowerCase();
+                            $('.city-wrap-list').append('<li><a name="'+ points +'" href="#' + link + '">' + key + '</a></li>');
+                        });
+                    }
+                });
+
+
+            });
+
+
+            //City list (should render after click on tradition)
+            var city = settings.belgium.city;
+
+            $('#block-belgium-map').append(cityWrap);
+            $.each(city, function (key, value) {
+                // var link = value.toLowerCase();
+                // $('.city-wrap-list').append('<li><a href="#' + link + '">' + value + '</a></li>');
+            });
 
         }
     };
+
     $(document).ready(function () {
-        google.load("visualization", "1", {packages:["geomap"]});
-        google.setOnLoadCallback(drawRegionsMap);
+        var post = "&trad_id=" + 1;
+        $.ajax({
+            'url': 'ajax/get_info',
+            'type': 'POST',
+            'dataType': 'json',
+            'data': post,
+            'success': function (data) {
+                $.each(data, function (key, value) {
+                    var link = key.toLowerCase();
+                    $('.city-wrap-list').append('<li><a href="#' + link + '">' + key + '</a></li>');
+                });
+            }
+        });
 
-        function drawRegionsMap() {
+        //map hightlight functional
+        $('img[usemap]').maphilight({ stroke: false, fillColor: '008800', fillOpacity: 0.7, wrapClass: 'map-wrap' });
 
-            var data = google.visualization.arrayToDataTable([
-                ['Region', 'Putten'],
-                ['Antwerp', 100],
-                ['Retie', 300],
-                ['Dessel', 300],
-                ['Mol', 300],
-                ['Meerhout', 300],
-                ['Geel', 300],
-                ['Laakdal', 300],
-                ['Veerle', 300],
+        $('a').click(function (e) {
+            e.preventDefault();
+            var hightLightId = $(this).text().replace('#', '').toLowerCase();
+            $('area').data('maphilight', data);
+            var data = $('#' + hightLightId).mouseout().data('maphilight') || {};
+            data.alwaysOn = !data.alwaysOn;
+            $('#' + hightLightId).data('maphilight', data).trigger('alwaysOn.maphilight');
+        });
 
-
-            ]);
-
-            var options = {
-                region: 'BE',
-                resolution: 'provinces',
-                displayMode : 'text'
-            };
-            options['dataMode'] = 'regions';
-
-           // var chart = new google.visualization.GeoChart(document.getElementById('regions_div'));
-            var container = document.getElementById('regions_div');
-            var geomap = new google.visualization.GeoMap(container)
-
-            geomap.draw(data, options);
-
-        }
-
+        // Image resize support
+        $('map').imageMapResize();
     });
 
 })(jQuery, Drupal, this, this.document);
